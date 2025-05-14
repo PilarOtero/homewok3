72d137b38999edcea61e8e06bf202b005e3b49bf
Pilar Otero <potero@udesa.edu.ar>
2025-05-14 17:23:00 -0300

    punto 2 creacion de clases

---
+++ punto 2/classes.h
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//PUNTO
+class Punto {
+    private:
+        double x;
+        double y;
+    public:
+        //Constructor
+        Punto(double x, double y);
+
+        //Getters
+        double getX() const;
+        double getY() const;
+
+        //Setters
+        void setX(double x_p);
+        void setY(double y_p);
+};
+
+//CIRCULO
+class Circulo {
+    private:
+        double radio;
+        Punto position;
+    
+    public:
+        //Constructor
+        Circulo(double radio_circle, Punto position_circle);
+
+        //Getters
+        double getRadio() const;
+        Punto getPosition() const { return position; }
+
+        //Setters 
+        void setRadio(double r);
+        void setPosition(Punto& p);
+};
+
+//ELIPSE
+class Elipse {
+    private:
+        Punto center;
+        //Semieje mayor
+        double a;
+        //Semieje menor
+        double b;
+    public:
+        //Constructor
+        Elipse(Punto center_e, double a_e, double b_e);
+
+        //Getters
+        Punto getCenter() const;
+        double getA() const;
+        double getB() const;
+
+        //Setters
+        void setCenter(Punto& p);
+        void setA(double major);
+        void setB(double minor); 
+};
+
+//RECTANGULO
+class Rectangulo {
+    private:
+        Punto left_inferior_vertex;
+        double width;
+        double length;
+    public:
+        //Constructor
+        Rectangulo(Punto left_inferior_vertex_r, double width_r, double length_r);
+
+        //Getters
+        Punto getLeftInferiorVertex() const;
+        double getWidth() const;
+        double getLength() const;
+
+        //Setters (this para referir a la instancia actual de la clase)
+        void setLeftInferiorVertex(Punto& p);
+        void setWidth(double w);
+        void setLength(double l);
+};
+
+//PROCESADOR DE FIGURAS
+//Caso generico
+template <typename T>
+class ProcesadorFiguras {
+    private:
+        T figure;
+    public:
+        //Constructor
+        ProcesadorFiguras(T figure_f);
+
+        //Getters
+        T getFigura() const;
+
+        //Setters
+        void setFigura(T& f);
+
+        //Metodo para calcular el area
+        double calculateArea();
+};
+
+//Caso especializacion Circulo
+template <>
+class ProcesadorFiguras<Circulo> {
+    private:
+        Circulo circle;
+
+    public:
+        //Constructor
+        ProcesadorFiguras(Circulo c1);
+
+        //Calculo del area
+        double calculateArea(Circulo& c);
+};
+
+//Caso especializacion Elipse
+template<>
+class ProcesadorFiguras<Elipse> {
+    private:
+        Elipse elipse;
+    public:
+        //Constructor
+        ProcesadorFiguras(Elipse e1);
+
+        //Calculo del area
+        double calculateArea(Elipse& e);
+};
+
+//Caso especializacion Rectangulo
+template<>
+class ProcesadorFiguras<Rectangulo> {
+    private:
+        Rectangulo rectangle;
+    public:
+        //Constructor
+        ProcesadorFiguras(Rectangulo r1);
+        //Calculo del area
+        double calculateArea(Rectangulo& r);
+};
---
+++ punto 2/main.cpp
@@ -0,0 +1,27 @@
+#include "classes.h"
+#include <iomanip>
+
+int main () {
+    //Creacion de puntos
+    Punto p1(10,20);
+    Punto circle_center(2.5,5.3);
+    Punto elipse_center(4.8,7.0);
+    Punto rectangle_infV(4.4,4.4);
+
+    //Creacion de figuras
+    Circulo c1(5.0, circle_center);
+    Elipse e1(elipse_center, 10.9, 15.6);
+    Rectangulo r1(rectangle_infV, 5.0, 10.333);
+
+    //Procesadores
+    ProcesadorFiguras<Circulo> processed_circle(c1);
+    ProcesadorFiguras<Elipse> processed_elipse(e1);
+    ProcesadorFiguras<Rectangulo> processed_rectangle(r1);
+
+    //Defino que el resultado tenga 3 cifras detras de la coma
+    cout << fixed << setprecision(3);
+    cout << "========AREAS========" << endl;
+    cout << "1. Circulo -> A =  " << processed_circle.calculateArea(c1) << endl;
+    cout << "2. Elipse -> A = " << processed_elipse.calculateArea(e1) << endl;
+    cout << "3. Rectangulo -> A = " << processed_rectangle.calculateArea(r1) << endl;
+}
